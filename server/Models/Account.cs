9e3411413fd800bc96844ac75bb5db18a0c6df9d
heather bickish <hbickish@gmail.com>
2025-01-16 14:10:32 -0700

    recipe and profile models

--- server/Models/Account.cs
+++ server/Models/Account.cs
@@ -2,8 +2,8 @@ namespace all_spice.Models;
 
 public class Account
 {
-  public string Id { get; set; }
-  public string Name { get; set; }
+  // public string Id { get; set; }
+  // public string Name { get; set; }
   public string Email { get; set; }
-  public string Picture { get; set; }
+  // public string Picture { get; set; }
 }
---
+++ server/Models/Recipe.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace all_spice.Models;
+
+public class Recipe
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  [MaxLength(255)] public string Title { get; set; }
+  [MaxLength(5000)] public string Instructions { get; set; }
+  [Url] public string Img { get; set; }
+  public string Catagory { get; set; }
+  public string CreatorId { get; set; }
+  public Profile Creator { get; set; }
+}
