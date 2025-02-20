cc8467a2b1104386196b9d0afa2d15c6f00af8dd
heather bickish <hbickish@gmail.com>
2025-02-20 12:32:07 -0700

    deploy edit button

--- client/src/components/RecipeDetailsModal.vue
+++ client/src/components/RecipeDetailsModal.vue
@@ -80,7 +80,7 @@ async function deleteRecipe(recipeId) {
                   <div>
                     <h4 class="text-success" id="recipeDetailsModalLabel">{{ recipe.title }}</h4>
                     <div v-if="recipe?.creatorId == account?.id" class="dropdown">
-                      <button class=" btn fs-4 ms-4 dropdown toggle" type="button" data-bs-toggle="dropdown"
+                      <button class=" btn fs-4 ms-4 dropdown toggle d-flex" type="button" data-bs-toggle="dropdown"
                         aria-expanded="false"><i class="mdi mdi-dots-horizontal"></i></button>
                       <ul class="dropdown-menu">
                         <li role="button" class="dropdown-item" title="Edit recipe" @click="editMode = !editMode">Edit
@@ -121,51 +121,6 @@ async function deleteRecipe(recipeId) {
                   </div>
                 </div>
               </div>
-              <!-- <div class="d-flex">
-                <img :src="recipe.img" alt="" class="recipe-box">
-                <div class="ms-4">
-                  <div class="d-flex align-items-center">
-                    <h4 class="text-success" id="recipeDetailsModalLabel">{{ recipe.title }}</h4>
-                    <div v-if="recipe?.creatorId == account?.id" class="dropdown">
-                      <button class=" btn fs-4 ms-4 dropdown toggle" type="button" data-bs-toggle="dropdown"
-                        aria-expanded="false"><i class="mdi mdi-dots-horizontal"></i></button>
-                      <ul class="dropdown-menu">
-                        <li role="button" class="dropdown-item" @click="editMode = !editMode">Edit Recipe</li>
-                        <li role="button" class="dropdown-item text-danger" @click="deleteRecipe(recipe.id)">Delete
-                          Recipe</li>
-                      </ul>
-                    </div>
-                  </div>
-                  <small>by: {{ recipe.creator?.name }}</small>
-                  <p class="mt-2 text-capitalize"><em>{{ recipe.category }}</em></p>
-                  <h5 class="mt-3 mb-3">Ingredients</h5>
-                  <div class="ingreds">
-                    <form @submit.prevent="createIngredients()">
-                      <input v-model="editableIngredients.quantity" v-if="editMode == true" class="form-control mb-2"
-                        placeholder="quantity..." type="text" name="quantity" id="quantity">
-                      <input v-model="editableIngredients.name" v-if="editMode == true" type="text" name="name"
-                        id="name" placeholder="ingredient..." class="form-control">
-                      <div class="text-end mt-2">
-                        <button type="submit" v-if="editMode == true"
-                          class="btn btn-sm btn-secondary mb-2">Save</button>
-                      </div>
-                    </form>
-                    <ul v-for="ingredient in ingredients" :key="ingredient.id">
-                      <li>{{ ingredient.quantity }} {{ ingredient.name }}</li>
-                    </ul>
-                  </div>
-                  <div>
-                    <h5>Instructions</h5>
-                    <textarea v-model="editableInstructions.instructions" v-if="editMode == true" type="text"
-                      class="form-control" maxlength="500"></textarea>
-                    <p v-else>{{ recipe.instructions }}</p>
-                    <div class="text-end mt-2">
-                      <button @click="editInstructions()" v-if="editMode == true"
-                        class="btn btn-sm btn-secondary">Save</button>
-                    </div>
-                  </div>
-                </div>
-              </div> -->
             </div>
             <div>
             </div>
